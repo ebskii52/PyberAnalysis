079108e9096e849466bd423b2916876e772a0478
ebskii52 <ebskii52@users.noreply.github.com>
2019-12-18 23:04:14 -0800

    message

--- PyBer.py
+++ PyBer.py
@@ -61,3 +61,281 @@ pyber_data_df = pd.merge(ride_data_df, city_data_df, on=["city", "city"])
 pyber_data_df.head()
 
 # %%
+# Create the Urban city DataFrame.
+urban_cities_df = pyber_data_df[pyber_data_df["type"] == "Urban"]
+urban_cities_df.head()
+
+# %%
+# Create the Suburban and Rural city DataFrames.
+suburban_cities_df = pyber_data_df[pyber_data_df["type"] == "Suburban"]
+rural_cities_df = pyber_data_df[pyber_data_df["type"] == "Rural"]
+
+# %%
+# Get the number of rides for urban cities.
+urban_ride_count = urban_cities_df.groupby(["city"]).count()["ride_id"]
+urban_ride_count.head()
+
+# %%
+# Create the suburban and rural ride count.
+suburban_ride_count = suburban_cities_df.groupby(["city"]).count()["ride_id"]
+
+rural_ride_count = rural_cities_df.groupby(["city"]).count()["ride_id"]
+
+# %%
+# Get average fare for each city in the urban cities.
+urban_avg_fare = urban_cities_df.groupby(["city"]).mean()["fare"]
+urban_avg_fare.head()
+
+# %%
+# Get average fare for each city in the suburban and rural cities.
+suburban_avg_fare = suburban_cities_df.groupby(["city"]).mean()["fare"]
+rural_avg_fare = rural_cities_df.groupby(["city"]).mean()["fare"]
+
+# %%
+# Get the average number of drivers for each urban city.
+urban_driver_count = urban_cities_df.groupby(["city"]).mean()["driver_count"]
+urban_driver_count.head()
+
+# %%
+# Get the average number of drivers for each city for the suburban and rural cities.
+suburban_driver_count = suburban_cities_df.groupby(["city"]).mean()["driver_count"]
+rural_driver_count = rural_cities_df.groupby(["city"]).mean()["driver_count"]
+
+# %%
+# Build the scatter plots for urban cities.
+plt.scatter(urban_ride_count, urban_avg_fare)
+
+# %%
+# Build the scatter plots for urban cities.
+plt.scatter(urban_ride_count,
+      urban_avg_fare,
+      s=urban_driver_count)
+
+# %%
+# Build the scatter plots for urban cities.
+plt.scatter(urban_ride_count,
+      urban_avg_fare,
+      s=10*urban_driver_count, c="coral",
+      edgecolor="black", linewidths=2,
+      alpha=0.8, label="Urban")
+plt.title("PyBer Ride-Sharing Data (2019)")
+plt.ylabel("Average Fare ($)")
+plt.xlabel("Total Number of Rides (Per City)")
+plt.grid(True)
+# Add the legend.
+plt.legend()
+
+# %%
+# Build the scatter plots for suburban cities.
+plt.scatter(suburban_ride_count,
+      suburban_avg_fare,
+      s=20*suburban_driver_count, c="skyblue",
+      edgecolor="black", linewidths=1,
+      alpha=0.8, label="Suburban")
+plt.title("PyBer Ride-Sharing Data (2019)")
+plt.ylabel("Average Fare ($)")
+plt.xlabel("Total Number of Rides (Per City)")
+plt.grid(True)
+# Add the legend.
+plt.legend()
+
+# %%
+# Build the scatter plots for rural cities.
+plt.scatter(rural_ride_count,
+      rural_avg_fare,
+      s=30*rural_driver_count, c="gold",
+      edgecolor="black", linewidths=2,
+      alpha=0.8, label="Rural")
+plt.title("PyBer Ride-Sharing Data (2019)")
+plt.ylabel("Average Fare ($)")
+plt.xlabel("Total Number of Rides (Per City)")
+plt.grid(True)
+# Add the legend.
+plt.legend()
+
+# %%
+# Add the scatter charts for each type of city.
+plt.scatter(urban_ride_count, 
+      urban_avg_fare, 
+      s=10*urban_driver_count, c="coral", 
+      edgecolor="black", linewidths=1, 
+      alpha=0.8, label="Urban")
+
+plt.scatter(suburban_ride_count, 
+      suburban_avg_fare, 
+      s=10*suburban_driver_count, c="skyblue", 
+      edgecolor="black", linewidths=1, 
+      alpha=0.8, label="Suburban")
+
+plt.scatter(rural_ride_count, 
+      rural_avg_fare, 
+      s=10*rural_driver_count, c="gold", 
+      edgecolor="black", linewidths=1, 
+      alpha=0.8, label="Rural")
+
+# Show the plot
+plt.show()
+
+# %%
+
+# Build the scatter charts for each city type.
+plt.subplots(figsize=(10, 6))
+plt.scatter(urban_ride_count, 
+      urban_avg_fare, 
+      s=10*urban_driver_count, c="coral", 
+      edgecolor="black", linewidths=1, 
+      alpha=0.8, label="Urban")
+
+plt.scatter(suburban_ride_count, 
+      suburban_avg_fare, 
+      s=10*suburban_driver_count, c="skyblue", 
+      edgecolor="black", linewidths=1, 
+      alpha=0.8, label="Suburban")
+
+plt.scatter(rural_ride_count, 
+      rural_avg_fare, 
+      s=10*rural_driver_count, c="gold", 
+      edgecolor="black", linewidths=1, 
+      alpha=0.8, label="Rural")
+
+# Incorporate the other graph properties
+plt.title("PyBer Ride-Sharing Data (2019)", fontsize=20)
+plt.ylabel("Average Fare ($)", fontsize=12)
+plt.xlabel("Total Number of Rides (Per City)", fontsize=12)
+plt.grid(True)
+
+# Add the legend.
+# Create a legend
+lgnd = plt.legend(fontsize="12", mode="Expanded",
+         scatterpoints=1, loc="best", title="City Types")
+lgnd.legendHandles[0]._sizes = [75]
+lgnd.legendHandles[1]._sizes = [75]
+lgnd.legendHandles[2]._sizes = [75]
+lgnd.get_title().set_fontsize(12)
+
+# Incorporate a text label about circle size.
+plt.text(42, 35, "Note:\nCircle size correlates\nwith driver count per city.", fontsize="12")
+
+#plt.legend()
+# Show the plot
+#plt.show()
+
+# Save the figure.
+plt.savefig("analysis/Fig1.png")
+
+# %%
+# Get summary statistics.
+urban_cities_df.describe()
+
+# %%
+# Get summary statistics.
+urban_ride_count.describe()
+
+# %%
+# Calculate the mean of the ride count for each city type.
+round(urban_ride_count.mean(),2), round(suburban_ride_count.mean(),2), round(rural_ride_count.mean(),2)
+
+# %%
+# Calculate the mode of the ride count for the urban cities.
+urban_ride_count.mode()
+
+# %%
+# Import NumPy and the stats module from SciPy.
+import numpy as np
+import scipy.stats as sts
+
+# Calculate the measures of central tendency for the ride count for the urban cities.
+mean_urban_ride_count = np.mean(urban_ride_count)
+print(f"The mean for the ride counts for urban trips is {mean_urban_ride_count:.2f}.")
+
+median_urban_ride_count = np.median(urban_ride_count)
+print(f"The median for the ride counts for urban trips is {median_urban_ride_count}.")
+
+mode_urban_ride_count = sts.mode(urban_ride_count)
+print(f"The mode for the ride counts for urban trips is {mode_urban_ride_count}.")
+
+
+
+
+# %%
+# Get the fares for the urban cities.
+urban_fares = urban_cities_df["fare"]
+urban_fares.head()
+
+# %%
+# Calculate the measures of central tendency for the average fare for the urban cities.
+mean_urban_fares = np.mean(urban_fares)
+print(f"The mean fare price for urban trips is ${mean_urban_fares:.2f}.")
+
+median_urban_fares = np.median(urban_fares)
+print(f"The median fare price for urban trips is ${median_urban_fares:.2f}.")
+
+mode_urban_fares = sts.mode(urban_fares)
+print(f"The mode fare price for urban trips is {mode_urban_fares}.")
+
+# %%
+# Get the driver count data from the urban cities.
+urban_drivers = urban_cities_df['driver_count']
+urban_drivers.head()
+
+# %%
+# Create a box-and-whisker plot for the urban cities ride count.
+x_labels = ["Urban"]
+fig, ax = plt.subplots()
+ax.boxplot(urban_ride_count, labels=x_labels)
+# Add the title, y-axis label and grid.
+ax.set_title('Ride Count Data (2019)')
+ax.set_ylabel('Number of Rides')
+ax.set_yticks(np.arange(10, 41, step=2.0))
+ax.grid()
+plt.show()
+
+# %%
+# Add all ride count box-and-whisker plots to the same graph.
+x_labels = ["Urban", "Suburban","Rural"]
+ride_count_data = [urban_ride_count, suburban_ride_count, rural_ride_count]
+fig, ax = plt.subplots(figsize=(10, 6))
+ax.set_title('Ride Count Data (2019)',fontsize=20)
+ax.set_ylabel('Number of Rides',fontsize=14)
+ax.set_xlabel("City Types",fontsize=14)
+ax.boxplot(ride_count_data, labels=x_labels)
+ax.set_yticks(np.arange(0, 45, step=3.0))
+ax.grid()
+# Save the figure.
+plt.savefig("analysis/Fig2.png")
+plt.show()
+
+# %%
+# Get the city that matches 39.
+urban_city_outlier = urban_ride_count[urban_ride_count==39].index[0]
+print(f"{urban_city_outlier} has the highest rider count.")
+
+# %%
+# Create a box-and-whisker plot for the urban fare data.
+x_labels = ["Urban"]
+fig, ax = plt.subplots()
+ax.boxplot(urban_fares, labels=x_labels)
+# Add the title, y-axis label and grid.
+ax.set_title('Ride Fare Data (2019)')
+ax.set_ylabel('Fare($USD)')
+ax.set_yticks(np.arange(0, 51, step=5.0))
+ax.grid()
+plt.show()
+print("Summary Statistics")
+urban_fares.describe()
+
+# %%
+# Create the box-and-whisker plot for the urban driver count data.
+x_labels = ["Urban"]
+fig, ax = plt.subplots()
+ax.boxplot(urban_drivers,labels=x_labels)
+# Add the title, y-axis label and grid.
+ax.set_title('Driver Count Data (2019)')
+ax.set_ylabel('Number of Drivers)')
+ax.set_yticks(np.arange(0, 90, step=5.0))
+ax.grid()
+plt.show()
+print("Summary Statistics")
+urban_drivers.describe()
+
+# %%
